1e4c8159ae34caafe9b27fb27b718c4d5d680327
toddhricik <toddhricik@gmail.com>
2024-12-06 00:05:43 +0000

    timeEmbedding_tests_tf1.py now works and can be debugged by the tensorflow debugger tfdbg in cli mode.

--- timeEmbedding_tests_tf1.py
+++ timeEmbedding_tests_tf1.py
@@ -1,17 +1,23 @@
 import sys
+import os
 import numpy as np
 
 import tensorflow.compat.v1 as tf
 import tensorflow.contrib as tf_contrib
 from tensorflow.python import debug as tf_debug
 
-import diffusion_tf.models.unet as reverseDiffusionModel
-import diffusion_tf.nn as 
+import diffusion_tf.models as models 
+import diffusion_tf.nn as nn 
 import diffusion_tf.diffusion_utils as du 
 from diffusion_tf import utils
 import diffusion_tf.diffusion_utils_2 as du2
 
+from diffusion_tf.diffusion_utils_2 import get_beta_schedule, GaussianDiffusion2
+import diffusion_tf.models.unet as unet
+
+
 if __name__ == "__main__":
+
     # Set the number of timesteps between x_0 and x_0 in the latent space
     numTimesteps = 1000
 
@@ -20,11 +26,11 @@ if __name__ == "__main__":
     fakeX = tf.zeros([10, 32, 32, 3])
     
     # Create a fake batch of y variables for labels
-    fakeY = tf.zeros([10])
+    fakeY = tf.zeros([10], dtype=tf.int32)
     
     # Define placeholders for inputs
-    x_placeholder = tf.placeholder(tf.float32, shape=[None, fakeBatch.shape[-3], fakeBatch.shape[-2], fakeBatch.shape[-3]])
-    y_placeholder = tf.placeholder(tf.float32, shape=[None, fakeBatch.shape[-3], fakeBatch.shape[-2], fakeBatch.shape[-3]])
+    x_placeholder = tf.placeholder(tf.float32, shape=[None, fakeX.shape[-3], fakeX.shape[-2], fakeX.shape[-1]])
+    y_placeholder = tf.placeholder(tf.float32, shape=[None, 1])
 
     # We need a vector of randomly chosen time steps
     t = tf.random_uniform([10], 0, numTimesteps, dtype=tf.int32)
@@ -32,11 +38,17 @@ if __name__ == "__main__":
     # We need a vector of betas which should be an np.ndarray
     betas = du2.get_beta_schedule(beta_schedule='linear', beta_start=10e-4, beta_end=0.02, num_diffusion_timesteps=1000)
     
+    # Set thenumber of classes
+    num_classes = 1 # tf.size(tf.unique(fakeY))
+
+    
+    # Create a diffusion model
+    diffusionModel = du2.GaussianDiffusion2(betas=betas, model_mean_type='eps', model_var_type='fixed', loss_type='mse')
     # Create the model for the reverse diffusion process as described by Ho et al. 2020 DPPM paper (arxiv:2006.11239).
-    reverseDiffusionModel = unet.Model(name="here", betas=betas, model_mean_type='eps', model_var_type='fixed', loss_type='mse', num_classes=1, dropout=0.1, randflip=1)
+    result = unet.model(x=fakeX, t=t, y=None, name="here", num_classes=num_classes, ch=128, ch_mult=(1,2,2,2), num_res_blocks=2, attn_resolutions=(16,), out_ch=256, dropout=0., resamp_with_conv=True)
 
     # Define or create the loss function
-    loss_fn = tf.square(tf.subtract(y_placeholder, estimated_targets))
+    loss_fn = tf.square(tf.subtract(y_placeholder, result))
 
     # Create a tensorflow 1.x session
     sess = tf.Session()
